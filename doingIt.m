ab0a1715b05f8e20603418f4e66d01aa82417d59
annikakaelin-bit <annikakaelin@icloud.com>
2025-10-30 14:23:35 -0400

    Add time tracking and condition for pulling prompts

--- doingIt.m
+++ doingIt.m
@@ -1,3 +1,10 @@
 data = 'pullPromt.csv'
-
-[title, quote, filename] = pullPromt(data)
+time = clock
+day = time(3)
+if day ~= daylast
+    daylast = 0
+else
+    [title, quote, filename] = pullPromt(data)
+    timelast = clock
+    daylast = timelast(3)
+end
